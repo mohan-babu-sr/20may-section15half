a8b1a46ed6685208d5a2cdc6c1d0a4018cad1e6b
MOHAN BABU S R <83107890+mohan-drive@users.noreply.github.com>
2021-05-20 19:14:38 +0530

    Add files via upload

--- Displaying a Map Using Leaflet Library/script.js
+++ Displaying a Map Using Leaflet Library/script.js
@@ -18,9 +18,28 @@ if(navigator.geolocation){
 //inside function (successCallback,errorCallback)
     navigator.geolocation.getCurrentPosition(
         function(position){
-            const{latitude}=position.coords;
-            const{longitude}=position.coords;
+            const {latitude}=position.coords; //destructuring - create variable latitude
+            const {longitude}=position.coords;
+            // console.log(latitude,longitude);
             console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+
+
+            const coords=[latitude,longitude];
+            
+            //Here we create a map in the 'map' div, add tiles of our choice, and then add a marker with some text in a popup  src(leaflet)
+            //L is namespace for couple of method
+            const map = L.map('map').setView(coords, 13);//zoom level of map by deafult
+
+
+            //theme
+            L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            L.marker(coords).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
         },
         function(){
             alert('Could not get your location');
